b1fafeb82327512dffe56a2cef8e12861ad27423
axelnq <axelnquintana@gmail.com>
2022-01-04 13:36:10 -0300

    Get countries with query

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -2,19 +2,15 @@ const { Router } = require('express');
 
 const { Country } = require('../db');
 
+const { Op } = require('sequelize');
+
 const axios = require('axios');
+
+const db = require('../db');
 
 const router = Router();
 
-
-router.get('/', async (req, res, next) => {
-    /*
-    Imagen de la bandera
-    Nombre
-    Continente
-    cantidad de población
-    */
-
+const getApiInfo = async () => {
     const apiUrl = await axios.get('https://restcountries.com/v3/all');
     const countriesApi = await apiUrl.data.map(country => {
         let capital = "";
@@ -28,43 +24,51 @@ router.get('/', async (req, res, next) => {
             capital:capital // No lo pide en la ruta principal pero es obligatorio para agregar a la base de datos
         }
     })
-    /*
-    countriesApi.map(async country => {
-        let newCountry = await Country.create({
-            id:country.id,
-            name:country.name,
-            continent:country.region,
-            population:country.population,
-            flagImage:country.flagImage,
-            capital:country.capital
-        })
-        return newCountry;
-    })
-    */
-  
-    countriesApi.forEach(async (country,index) => {
-       await Country.create({
-           ...country
-       })
-    })
+    return countriesApi;
+}
 
+router.get('/', async (req, res, next) => {
+    /*Imagen de la bandera ,Nombre , Continente ,cantidad de població */
+
+    const {name} = req.query;
+
+    const apiCountries = await getApiInfo();
+
+    try {
+        // Si tengo la db con info no hago nada
+        var dbCountries = await Country.findAll();
+        // Si no tengo datos los creo
+        if(!dbCountries.length) {
+            await Country.bulkCreate(apiCountries);
+            dbCountries = await Country.findAll();
+        }
+    } catch(error) {
+        next(error);
+    }
+
+    if(!name) return res.status(200).json(dbCountries.map(country => country.name));
+
+    try {
+        let country = await Country.findAll({
+            where: { 
+                name : {
+                    [Op.iLike] : '%' + name + '%' // iLike ignora mayusculas y minisculas y los porcentajes buscan name como substring 
+                }
+            }
+        })
+        console.log(country);
+        if(Object.keys(country).length === 0) return res.status(400).send({message: "This country doesn't exist."})
+
+        return res.status(200).json(country);
+    } catch (error) {
+        next(error);
+    }
     
-    // antartica index 230 , no tiene capital
-    res.send('soy get /countries');
 })
 
 router.post('/',  (req, res, next) => {
     res.send('soy post /countries');
 })
-
-router.delete('/',  (req, res, next) => {
-    res.send('soy delete /countries');
-})
-
-router.put('/',  (req, res, next) => {
-    res.send('soy delete /countries');
-})
-
 
 
 module.exports = router;
